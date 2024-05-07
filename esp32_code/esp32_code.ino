752865ea36c2e2a2bb0fcee623760512fcd21ba1
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2024-05-07 07:36:57 -0300

    codigo exemplo do esp32

--- esp32_code/esp32_code.ino
+++ esp32_code/esp32_code.ino
@@ -1,1 +1,137 @@
+// Autor: DjEdu28 <GitHub.com/DjEdu28>
+// # Como configurar o ESP32 no Arduino IDE
+// https://www.crescerengenharia.com/post/instalando-esp32-arduino
+// # Config_Arduino https://dl.espressif.com/dl/package_esp32_index.json
+//
+// # Projetos WOKWI
+// ESP32 com DHT22 by DjEdu28 https://wokwi.com/projects/397207107109591041
 // https://wokwi.com/projects/397128466983540737
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+
+// Substitua com suas credenciais de WiFi
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+// Substitua com o endereço do seu broker MQTT
+const char* mqtt_server = "test.mosquitto.org";
+const char* clientId = "esp32_djedu28_397207107109591041";
+
+const char* TOPIC_temperature = "esp32_djedu28_temperature";
+const char* TOPIC_humidity = "esp32_djedu28_humidity";
+const char* TOPIC_ledControl = "esp32_djedu28_ledControl";;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Substitua o pino do LED
+#define LED_PIN 15
+
+// Substitua o pino do sensor DHT
+#define DHTPIN 4
+// DHTTYPE DHT22 ou DHT11
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  dht.begin();
+  digitalWrite(LED_PIN, LOW);
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println("");
+  Serial.print("Conectando... ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password, 6);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print(" WiFi Conectado!");
+  Serial.print(" IP: ");
+  Serial.println(WiFi.localIP());
+}
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Tentando conectar MQTT ... ");
+    if (client.connect(clientId)) {
+      Serial.println("conectado");
+      client.subscribe(TOPIC_ledControl);
+    } else {
+      Serial.print("falhou, rc =");
+      Serial.print(client.state());
+      Serial.println(" Tente novamente em 5 segundos");
+      delay(5000);
+    }
+  }
+}
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Falha ao ler o sensor DHT!");
+    return;
+  }
+
+  if (millis() - tempo_ans >= 5000) {
+    //Publica os dados de temperatura e umidade
+    client.publish(TOPIC_temperature, String(temperature).c_str());
+    client.publish(TOPIC_humidity, String(humidity).c_str());
+    if (digitalRead(PIN_LED)) {
+      client.publish(TOPIC_ledControl, String("ON").c_str());
+    } else {
+      client.publish(TOPIC_ledControl, String("OFF").c_str());
+    }
+  }
+
+
+  Serial.print("TEMP\t");
+  Serial.print(temperature);
+  Serial.print("C\t");
+
+  Serial.print("UMID\t");
+  Serial.print(temperature);
+  Serial.println("%");
+
+  delay(500);
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+    messageTemp += (char)message[i];
+  }
+  Serial.print("Mensagem recebida[");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.println(messageTemp);
+
+  if (String(topic) == TOPIC_ledControl) {
+    if (messageTemp == "ON") {
+      digitalWrite(LED_PIN, HIGH);
+    } else if (messageTemp == "OFF") {
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+}
